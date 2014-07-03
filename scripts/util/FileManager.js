935ed76237d0a9132c7455f96a931e8032e747f1
Andrew Palmer <andyp.123@gmail.com>
2014-07-03 17:03:36 +0900

    Small changes
    + FileManager.js should be named filemanager.js
      - however, it’s not…

--- scripts/util/FileManager.js
+++ scripts/util/FileManager.js
@@ -1,14 +1,10 @@
-/**
-* Quake Web Tools Application.
-*
-* @module QuakeWebTools
-*/
 var QuakeWebTools = QuakeWebTools || {};
 
 /*
 TODO:
 + add local file upload support
-+ store files in the filemanager (path, arraybuffer, loaded file in typed container)
++ store files in the filemanager (path, arraybuffer, loaded file in typed
+  container)
 + allow downloading files
 + allow load/unload operations? (leak prone?)
 + drag and drop file support (dropzone.js? etc.)
@@ -19,12 +15,12 @@ html5 upload files with button
 */
 
 /**
-* A file manager that allows the user to upload, load (into a project etc.) and download
-* files.
+* A file manager that allows the user to upload, load (into a project etc.) and
+* download files.
 * @constructor
 */
 QuakeWebTools.FileManager = function() {
-    this.files = {};
+  this.files = {};
 }
 
 QuakeWebTools.FileManager.prototype.loadFile = function(path) {
